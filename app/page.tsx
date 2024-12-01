9456dd25312f696ad363c75181458f704c49f660
Zheng Jiawen <zhengjiawen44@gmail.com>
2024-12-01 11:44:52 +0800

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 import styles from "@/app/ui/home.module.css";
 import clsx from "clsx";
+import Counter from "./components/Counter";
 import { useState } from "react";
 const Home = () => {
-  const [color, setColor] = useState(true);
   return (
-    <>
-      <div
-        onClick={() => setColor(!color)}
-        className={clsx(color ? "bg-slate-400" : "bg-black", styles.shape)}
-      ></div>
-    </>
+    <div>
+      <Counter />
+    </div>
   );
 };
 
