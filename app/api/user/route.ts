230a0c3fb134f2b3fefedb27731ac7b1fc1fe331
Zheng Jiawen <zhengjiawen44@gmail.com>
2024-12-26 23:11:38 +0800

    user api

---
+++ app/api/user/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+export async function getUser(req: NextRequest) {
+  //get token
+  //verify token
+  //query database
+  //return user object
+}
